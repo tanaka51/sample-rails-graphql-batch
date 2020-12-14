7de33a36a60741d4e689ec72d814ed289297c8ee
Koichi Tanaka <tanaka51.jp@gmail.com>
2020-12-14 20:58:57 +0900

    Remove unneeded column

--- app/graphql/types/article_type.rb
+++ app/graphql/types/article_type.rb
@@ -3,7 +3,6 @@ module Types
     field :id, ID, null: false
     field :title, String, null: false
     field :content, String, null: false
-    field :user_id, Integer, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
     field :author, Types::UserType, null: true
