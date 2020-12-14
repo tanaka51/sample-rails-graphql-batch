00f06cebb8aa54c723ab4493f22957174bea3ff1
Koichi Tanaka <tanaka51.jp@gmail.com>
2020-12-14 20:42:18 +0900

    Setup seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+bob, alice = User.create!([
+  { name: 'Bob'},
+  { name: 'Alice'},
+])
+
+articles = Article.create!([
+  { title: "Bob's first article", content: 'first article', author: bob },
+  { title: "Bob's second article", content: 'second article', author: bob },
+  { title: "Bob's third article", content: 'third article', author: bob },
+
+  { title: "Alice's first article", content: 'first article', author: alice },
+  { title: "Alice's second article", content: 'second article', author: alice },
+  { title: "Alice's third article", content: 'third article', author: alice }
+])
+
+bob.favorites.create!(article: articles[3])
+bob.favorites.create!(article: articles[4])
+alice.favorites.create!(article: articles[0])
+alice.favorites.create!(article: articles[1])
